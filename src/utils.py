fbe15b4910a3e70d2e7b99c3ccf18dc2de150358
ezrahuffman <joshooha@gmail.com>
2025-04-28 12:20:44 -0700

    split text nodes into link and image nodes

--- src/utils.py
+++ src/utils.py
@@ -53,7 +53,7 @@ def split_nodes_delimiter(old_nodes:List[TextNode], delimiter:str, text_type:Tex
             text = text[second_index+1:]
     return new_nodes
 
-def extract_markdown_images(text:str):
+def extract_markdown_images(text:str) -> List[tuple]:
     matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
     #print(matches)
     # ret_lst = []
@@ -65,7 +65,7 @@ def extract_markdown_images(text:str):
     #     ret_lst.append(alt_text, url_text)
     return matches
 
-def extract_markdown_links(text:str):
+def extract_markdown_links(text:str) -> List[tuple]:
     matches = re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
     # ret_lst = []
     # for match in matches:
@@ -76,3 +76,38 @@ def extract_markdown_links(text:str):
     #     ret_lst.append(link_text, url_text)
     return matches
 
+def split_nodes_image(old_nodes:List[TextNode]) -> List[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        links = extract_markdown_images(node.text)
+        for link_info in links:
+            search_str = f"![{link_info[0]}]({link_info[1]})"
+            split = node.text.split(search_str, 1)
+            new_nodes.append(TextNode(split[0], TextType.TEXT))
+            if len(split) > 1:
+                new_nodes.append(TextNode(link_info[0], TextType.IMAGE, link_info[1]))
+                node.text = split[1]
+
+        if len(links) == 0:
+            new_nodes.append(node)
+        elif node.text != "":
+            new_nodes.append(TextNode(node.text, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes:List[TextNode]) -> List[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        links = extract_markdown_links(node.text)
+        for link_info in links:
+            search_str = f"[{link_info[0]}]({link_info[1]})"
+            split = node.text.split(search_str, 1)
+            new_nodes.append(TextNode(split[0], TextType.TEXT))
+            if len(split) > 1:
+                new_nodes.append(TextNode(link_info[0], TextType.LINK, link_info[1]))
+                node.text = split[1]
+
+        if len(links) == 0:
+            new_nodes.append(node)
+        elif node.text != "":
+            new_nodes.append(TextNode(node.text, TextType.TEXT))
+    return new_nodes
